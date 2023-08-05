b4f430b7f6a2abe900999ece92dae39176f6cc3b
Abraham Aleman <126976166+AbrahamAleman179@users.noreply.github.com>
2023-08-05 14:06:00 -0400

    Add files via upload

---
+++ src/solution.js
@@ -0,0 +1,10 @@
+function getUserName(Name) {
+  Name = "Abraham"
+  return result;
+}
+
+////////////////////////////////
+//// Ignore code below here.
+////////////////////////////////
+
+module.exports = getUserName;
